4b44b41962422c690a52ba0dfc8bfcd87e98c975
Babalola Paul <pbabalola27@gmail.com>
2026-08-12 16:48:27 +0100

    day one

--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,13 @@ const path = require('path')
 const rootDir = require('../util/path')
 const express = require('express')
 const router = express.Router()
+const adminData = require('./admin')
 
 router.get('/', (req, res, next) => {
-    // console.log("another middleware")
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    // console.log(adminData.products)
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    const products = adminData.products
+    res.render('shop', { prods: products, docTitle: 'shop' })
 })
 
 module.exports = router;
